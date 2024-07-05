b53e5ff6cefe9f717837252a37319af8c396b6e0
Your Name <you@example.com>
2024-07-05 16:48:18 +0300

    Transportation App Task

---
+++ c-sharp-apps-mark-kotlobay/TransportationApp/Bus.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_mark_kotlobay.TransportationApp
+{
+    public class Bus : PublicVehicle
+    {
+        private readonly int doors;
+        private bool bellStop = false;
+
+        public Bus() { }
+        public Bus(int doors, bool bellStop, int line, int id, int maxSpeed, int seats) : base(line, id, maxSpeed, seats)
+        {
+            this.doors = doors;
+            this.bellStop = bellStop;
+        }
+        public int Doors { get => doors; }
+        public bool BellStop { get => bellStop; set => bellStop = value; }
+
+        public override int MaxSpeed { get => maxSpeed; set { if (value <= 120) maxSpeed = value; } }
+
+        private int GetMaxPassengers()
+        {
+            return (int)Math.Round(Seats * 1.1);
+        }
+        private bool CalculateHasRoom()
+        {
+            if (CurrentPassengers < GetMaxPassengers())
+            {
+                HasRoom = true;
+                return true;
+            }
+            HasRoom = false;
+            return false;
+        }
+        public override void UploadPassengers(int num)
+        {
+            int rejectedPassengers;
+            if (CalculateHasRoom() == true)
+            {
+                if (num + CurrentPassengers > GetMaxPassengers())
+                {
+                    int temp = GetMaxPassengers() - CurrentPassengers;
+                    rejectedPassengers = num - temp;
+                    CurrentPassengers = GetMaxPassengers();
+                }
+                else
+                {
+                    CurrentPassengers += num;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        public override void ToString()
+        {
+            Console.WriteLine("Line: " + Line + " ,ID: " + Id + " ,Max speed: " + MaxSpeed + " ,Current passengers: " + CurrentPassengers + " ,Seats: " + Seats + " ,Has room: " + CalculateHasRoom() + " ,Doors: " + Doors + " ,Bellstop: " + BellStop);
+        }
+    }
+}
---
+++ c-sharp-apps-mark-kotlobay/TransportationApp/PassengersAirplain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_mark_kotlobay.TransportationApp
+{
+    public class PassengersAirplain : PublicVehicle
+    {
+        private int enginesNum;
+        private int wingLength;
+        private int rows;
+        private int columns;
+
+        public PassengersAirplain() { }
+        public PassengersAirplain(int enginesNum, int wingLength, int rows, int columns, int line, int id) : base(line, id)
+        {
+            this.enginesNum = enginesNum;
+            this.wingLength = wingLength;
+            this.rows = rows;
+            this.columns = columns;
+            Line = line;
+            Id = id;
+            Seats = Rows * Columns;
+        }
+        public int EnginesNum { get => enginesNum; set => enginesNum = value; }
+        public int WingLength { get => wingLength; set => wingLength = value; }
+        public int Rows { get => rows; set => rows = value; }
+        public int Columns { get => columns; set => columns = value; }
+        public override int MaxSpeed { get => maxSpeed; set { if (value <= 1000) maxSpeed = value; } }
+        private int GetMaxPassengers() { return Seats - 7; }
+        private bool CalculateHasRoom()
+        {
+            if (CurrentPassengers < GetMaxPassengers())
+            {
+                HasRoom = true;
+                return true;
+            }
+            HasRoom = false;
+            return false;
+        }
+        public override void UploadPassengers(int num)
+        {
+            int rejectedPassengers;
+            if (CalculateHasRoom() == true)
+            {
+                if (num + CurrentPassengers > GetMaxPassengers())
+                {
+                    int temp = GetMaxPassengers() - CurrentPassengers;
+                    rejectedPassengers = num - temp;
+                    CurrentPassengers = GetMaxPassengers();
+                }
+                else
+                {
+                    CurrentPassengers += num;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        public override void ToString()
+        {
+            Console.WriteLine("Line: " + Line + " ,ID: " + Id + " ,Max speed: " + MaxSpeed + " ,Current passengers: " + CurrentPassengers + " ,Seats: " + Seats + " ,Has room: " + CalculateHasRoom() + " ,Engines num: " + EnginesNum + " ,Wing length: " + WingLength + " ,Rows: " + Rows + " ,Columns: " + Columns);
+        }
+    }
+}
